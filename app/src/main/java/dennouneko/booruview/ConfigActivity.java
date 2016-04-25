2cb191cca1aaa1e8ed8c106436cecbd086ab372c
DennouNeko <remigiusz.banert@gmail.com>
2016-04-25 21:20:57 +0200

    Add "Clear Cache" button to settings.

--- app/src/main/java/dennouneko/booruview/ConfigActivity.java
+++ app/src/main/java/dennouneko/booruview/ConfigActivity.java
@@ -1,6 +1,7 @@
 package dennouneko.booruview;
 import android.os.*;
 import android.preference.*;
+import android.widget.*;
 
 public class ConfigActivity extends PreferenceActivity
 {
@@ -11,6 +12,21 @@ public class ConfigActivity extends PreferenceActivity
 		// update the config view with xml data
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		findPreference("pref_purgeBtn").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference p1) {
+				DataProvider.getInstance(getApplicationContext()).clearCache();
+				String msg = getResources().getString(R.string.msgPurged);
+				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+				updateCacheInfo();
+				return true;
+			}
+		});
+		
+		updateCacheInfo();
+	}
+	
+	private void updateCacheInfo() {
 		StringBuilder info = new StringBuilder();
 		ImageCache cache = DataProvider.getInstance(getApplicationContext()).cache;
 		info.append(String.format("Used: %s", BooruViewActivity.formatSize(cache.getUsedSpace(cache.mDir))));
